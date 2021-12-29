4413e99eba35570709ad9fcf689ee435b1a8be92
leanhquanvn97 <leanhquanvn97@gmail.com>
2021-12-29 16:55:14 +0700

    add localStorage

--- src/index.js
+++ src/index.js
@@ -10,7 +10,28 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import reducers from './redux/reducers'
 import App from './App';
 
-const store = createStore(reducers);
+function saveToLocalStorage(state) {
+    try {
+        const serialisedState = JSON.stringify(state);
+        localStorage.setItem("persistantState", serialisedState);
+    } catch (e) {
+        console.warn(e);
+    }
+}
+function loadFromLocalStorage() {
+    try {
+        const serialisedState = localStorage.getItem("persistantState");
+        if (serialisedState === null) return undefined;
+        return JSON.parse(serialisedState);
+    } catch (e) {
+        console.warn(e);
+        return undefined;
+    }
+}
+
+const store = createStore(reducers,loadFromLocalStorage());
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 const client = new ApolloClient({
     uri: 'http://localhost:4000',
